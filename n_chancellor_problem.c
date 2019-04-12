6acb62a7145590c39f7d6b3b00eb3ec69f7410bb
nicojelo <nlmercado@up.edu.ph>
2019-04-13 01:41:26 +0800

    working till btm=initial_populate + printing functions for [btm, nsiblings] for debugging

--- n_chancellor_problem.c
+++ n_chancellor_problem.c
@@ -1,78 +1,99 @@
-#define EMPTY 0			//space
-#define CHANCELLOR 1		//c
+// CONSTANTS
+#define EMPTY 0			//_
+#define CHANCELLOR 1	//c
 #define TAKEN 2			//-
 #define CHILD 3			//o
-#define INCEST 4			//x
+#define INCEST 4		//x
 #define TRUE 1			
-#define FALSE 0			
+#define FALSE 0		
+
+// LIBRARIES 
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-typedef struct puzzle{
+// USER-DEFINED STRUCTURES
+typedef struct puzzle_node{
     int N;
     int hasSpace;
-    int ** config; //2d array
-
+    int ** board; //2d array
 }PUZZLE;
 
-void initialize_btm(PUZZLE *** btm,int n){
+void initialize_btm(PUZZLE **** btm, int n){
 	int i;
 
-	(*btm) = (PUZZLE **) malloc(sizeof(PUZZLE *)*n);
+	(*btm) = (PUZZLE ***) malloc(sizeof(PUZZLE **)*n);
 	for(i=0;i<n;i++){
-		(*btm)[i] = (PUZZLE *) malloc(sizeof(PUZZLE)*n);
+		(*btm)[i] = (PUZZLE **) malloc(sizeof(PUZZLE *)*n);
 	}
 
 }
 
-void initialize_puzzle(PUZZLE ** init_config, int n){
+void initialize_puzzle_node(PUZZLE ** init_config, int n){
 	int i,j;
 
 	// initialize node
 	(*init_config) = (PUZZLE*) malloc(sizeof(PUZZLE));
 	(*init_config)->N = n;
-	(*init_config)->hasSpace = FALSE;
+	(*init_config)->hasSpace = TRUE;
 		// initialize config
-	(*init_config)->config = (int **) malloc(sizeof(int *)*n);
+	(*init_config)->board = (int **) malloc(sizeof(int *)*n);
 	for(i=0;i<n;i++){
-		(*init_config)->config[i] = (int *) malloc(sizeof(int)*n);
+		(*init_config)->board[i] = (int *) malloc(sizeof(int)*n);
 	}
 
 	for (i=0;i<n;i++){
 		for (j=0;j<n;j++){
-			(*init_config)->config[i][j] = EMPTY;
+			(*init_config)->board[i][j] = EMPTY;
 		}
 	}
 }
 
-void populate(PUZZLE *parent, PUZZLE **btm, int * nsiblings, int * curr_stack, int * curr_node){
+// given x, y as coordinates of C, fill up vulnerable spaces of board
+void fill_taken(PUZZLE * puzzle_node, int x, int y){
+	int n = puzzle_node->N;
+	int i,j;
+	// horizontal dashes
+	for(j=0;j<n;j++){
+		if(puzzle_node->board[x][j] == EMPTY)
+			puzzle_node->board[x][j] = TAKEN;
+	}
+	// vertical dashes
+	for(i=0;i<n;i++){
+		if(puzzle_node->board[i][y] == EMPTY)
+			puzzle_node->board[i][y] = TAKEN;
+	}
+	// knight movement
+	if(x-1>=0 && y-2>=0 && puzzle_node->board[x-1][y-2] == EMPTY)
+		puzzle_node->board[x-1][y-2] = TAKEN;
+	if(x-2>=0 && y-1>=0 && puzzle_node->board[x-2][y-1] == EMPTY)
+		puzzle_node->board[x-2][y-1] = TAKEN;
+	if(x+1<n && y+2<n && puzzle_node->board[x+1][y+2] == EMPTY)
+		puzzle_node->board[x+1][y+2] = TAKEN;
+	if(x+2<n && y+1<n && puzzle_node->board[x+2][y+1] == EMPTY)
+		puzzle_node->board[x+2][y+1] = TAKEN;
+	if(x-1>=0 && y+2<n && puzzle_node->board[x-1][y+2] == EMPTY)
+		puzzle_node->board[x-1][y+2] = TAKEN;
+	if(x-2>=0 && y+1<n && puzzle_node->board[x-2][y+1] == EMPTY)
+		puzzle_node->board[x-2][y+1] = TAKEN;
+	if(x+1<n && y-2>=0 && puzzle_node->board[x+1][y-2] == EMPTY)
+		puzzle_node->board[x+1][y-2] = TAKEN;
+	if(x+2<n && y-1>=0 && puzzle_node->board[x+2][y-1] == EMPTY)
+		puzzle_node->board[x+2][y-1] = TAKEN;
+
+}
+
+void checkif_hasSpace(PUZZLE * parent){
 	int i,j;
 	int n = parent->N;
-	int firstSpacei = -1;
-	int firstSpacej = -1;
-	// btm[*curr_stack][*curr_node] iS JUST ANAK
 
-	while (parent->hasSpace){
-		//make_anak
-		btm[*curr_stack][*curr_node] = (PUZZLE *) malloc(sizeof(PUZZLE));
-		initialize_puzzle(&btm[curr_stack][curr_node], n);
-		for(i=0;i<n;i++){
-			for(j=0;j<n;j++){	
-				if(parent->config[i][j] == CHANCELLOR)
-					btm[*curr_stack][*curr_node]->config[i][j] = CHANCELLOR;
-				else if(parent->config[i][j] == TAKEN)
-					btm[*curr_stack][*curr_node]->config[i][j] = TAKEN;
-				else if(firstSpacei == -1 && parent->config[i][j] == EMPTY)
-					firstSpacei = i;
-					firstSpacej = j;
-			}
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(parent->board[i][j] == EMPTY)
+				return;
 		}
-		btm[*curr_stack][*curr_node]->config[firstSpacei][firstSpacej] = CHANCELLOR;
-		parent->config[firstSpacei][firstSpacej] = CHILD;
-
 	}
+
+	parent->hasSpace = FALSE;
 }
 
 void print_puzzle_node(PUZZLE * node){
@@ -80,7 +101,7 @@ void print_puzzle_node(PUZZLE * node){
 
 	for(i=0;i<node->N;i++){
 		for(j=0;j<node->N;j++){
-			cell = node->config[i][j];
+			cell = node->board[i][j];
 			if(cell == EMPTY)
 				printf("_ ");
 			if(cell == CHANCELLOR)
@@ -96,27 +117,140 @@ void print_puzzle_node(PUZZLE * node){
 	}
 }
 
+void populate(PUZZLE *parent, PUZZLE ***btm, int * nsiblings, int * curr_stack, int * curr_node){
+	int i,j;
+	int n = parent->N;
+	int firstSpacei = -1;
+	int firstSpacej = -1;
+	PUZZLE * new_puzzle_ptr; // child
+
+	while (parent->hasSpace){
+		//make_anak
+		initialize_puzzle_node(&new_puzzle_ptr, n); // create node
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				// finding coordinates of first empty space
+				if(firstSpacei == -1 && parent->board[i][j] == EMPTY){
+					firstSpacei = i;
+					firstSpacej = j;
+				}
+				// copy C's and -'s of parent to child
+				if(parent->board[i][j] == CHANCELLOR)
+					new_puzzle_ptr->board[i][j] = CHANCELLOR;
+				else if(parent->board[i][j] == TAKEN)
+					new_puzzle_ptr->board[i][j] = TAKEN;
+			}
+		}
+		// update parent and child boards
+		new_puzzle_ptr->board[firstSpacei][firstSpacej] = CHANCELLOR;
+		parent->board[firstSpacei][firstSpacej] = CHILD;
+		fill_taken(new_puzzle_ptr, firstSpacei, firstSpacej);
+		// update parent according to incest rule
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(new_puzzle_ptr->board[i][j] == EMPTY)
+					parent->board[i][j] = INCEST;
+			}
+		}
+
+		// connect new child to btm
+		btm[*curr_stack][nsiblings[(*curr_stack)]] = new_puzzle_ptr;
+		// update stack pointers
+		nsiblings[(*curr_stack)]++;
+		// reset first free coordinates
+		firstSpacei = -1;
+		checkif_hasSpace(parent);
+	}
+	(*curr_stack)++;
+}
+
+void initialize_nsiblings(int ** nsiblings, int n){
+	int i;
+	// instantiate
+	(*nsiblings) = (int *) malloc(sizeof(int)*n);
+	// initialize values
+	for(i=0;i<n;i++){
+		(*nsiblings)[i] = 0;
+	}
+}
+
+void print_btm(PUZZLE * init_config, PUZZLE *** btm, int * nsiblings){
+	int n = init_config->N;
+	int layer, i, j, sib;
+	int cell;
+
+	printf("\nBacktracking Matrix\n");
+	
+	// for each layer
+	for(layer=0;layer<n;layer++){
+		// line break
+		for(i=0;i<n*(n+1)*2;i++){
+			printf("-");
+		}
+		printf(" %d", nsiblings[layer]);
+		printf("\n");
+		// for each row of whole layer
+		for(i=0;i<n;i++){
+			// for each cases in that layer
+			for(sib=0;sib<nsiblings[layer];sib++){
+				// for each cell across all cases
+				for(j=0;j<n;j++){
+					cell = btm[layer][sib]->board[i][j];
+					switch(cell){
+						case EMPTY:
+							printf("  ");
+							break;
+						case CHANCELLOR:
+							printf("C ");
+							break;
+						case TAKEN:
+							printf("- ");
+							break;
+						case CHILD:
+							printf("o ");
+							break;
+						case INCEST:
+							printf("x ");
+							break;
+					}
+				}
+				printf("| ");
+			}
+			printf("\n");
+		}
+	}
+	for(i=0;i<n*(n+1)*2;i++){
+		printf("-");
+	}
+	printf("\n");
+
+}
+
+void print_nsiblings(int * nsiblings, int n){
+	int layer;
+
+	printf(" ___ \n");
+	for(layer=0;layer<n;layer++){
+		printf("| %d |\n", nsiblings[layer]);
+	}
+	printf(" --- \n");
+}
 
 int main(){
-	// int num_p;
-	// puzzle_container [num_p];
-
-	//VARIABLES 
-
+	// VARIABLES 
 	int start = 0;
 	int curr_stack = 0;
 	int curr_node = 0;
 	int n = 4;
 	PUZZLE * init_config;
-	PUZZLE ** btm; //backtracking matrix
-	int nsiblings[n];
+	PUZZLE *** btm; //backtracking matrix (2d array of pointers)4
+	int * nsiblings;
 
-	initialize_puzzle(&init_config, n);
-	print_puzzle_node(init_config);
-
+	initialize_puzzle_node(&init_config, n);
+	initialize_nsiblings(&nsiblings, n);
 	initialize_btm(&btm,n);
-	populate(init_config,btm,&nsiblings,&curr_stack,&curr_node);
-	print_puzzle_node(btm[0][0]);
+	populate(init_config,btm,nsiblings,&curr_stack,&curr_node);
+	print_btm(init_config, btm, nsiblings);
 
 	return 0;
 }
