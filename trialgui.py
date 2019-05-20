d8440923c8c39053ea5904ccd8a440a337c6f71d
nicojelo <nlmercado@up.edu.ph>
2019-05-20 21:44:47 +0800

    fixed about section in trialgui

--- trialgui.py
+++ trialgui.py
@@ -7,7 +7,15 @@ window.title("N-Chancellor Solver")
 
 # BACKGROUND IMAGE
 bg = ImageTk.PhotoImage(Image.open("images/home.jpg"))
-menu = Canvas(window, width=768,height=768)
+abtimg = ImageTk.PhotoImage(Image.open("images/aboutpage.jpg"))
+startImage = PhotoImage(file="images/start.png")
+aboutImage = PhotoImage(file="images/abtbtn.png")
+exitImage = PhotoImage(file="images/abtbtn.png")
+goBackImage = PhotoImage(file="images/goback.png")
+
+homeFrame = Frame(window)
+homeFrame.grid(row=0, column=0, sticky='news')
+menu = Canvas(homeFrame, width=768,height=768)
 menu.pack()
 menu.create_image(384,384,image=bg)
 
@@ -16,33 +24,29 @@ def exit():
     window.destroy()
 
 def about():
-    menu.pack_forget()
     f2= Frame(window)
-    abtimg = ImageTk.PhotoImage(Image.open("images/aboutpage.jpg"))
     lbl = Label(f2,image=abtimg)
     lbl.pack()
 
-    # btn1 = Button(f2,text="Back To Menu",command=lambda: back(f3))
-    # btn1.pack()
-    f2.pack()
+    btn1 = Label(f2,width=266,height=92,image=goBackImage)
+    btn1.place(relx=1, x=-2, y=2, anchor=NE)
+    f2.grid(row=0, column=0, sticky='news')
 
 
 
-photo1 = PhotoImage(file="images/start.png")
-photo2 = PhotoImage(file="images/abtbtn.png")
-photo3 = PhotoImage(file="images/abtbtn.png")
-
-start = Button(menu,width=180,height=60,image=photo1,text="START")
-about = Button(menu,width=180,height=60,image=photo2,text="ABOUT",command=about)
-exit = Button(menu,width=180,height=60,image=photo3,text="EXIT",command=exit)
 
 
-# about.pack()
-# exit.pack()
+startButton = Button(menu,width=180,height=60,image=startImage,text="START")
+aboutButton = Button(homeFrame,width=180,height=60,image=aboutImage,text="ABOUT",command=about)
+exitButton = Button(menu,width=180,height=60,image=exitImage,text="EXIT",command=exit)
 
-start_button_window = menu.create_window(100, 630, anchor='nw', window=start)
-about_button_window = menu.create_window(300, 630, anchor='nw', window=about)
-quit_button_window = menu.create_window(500, 630, anchor='nw', window=exit)
+
+aboutButton.pack()
+exitButton.pack()
+
+start_button_window = menu.create_window(100, 630, anchor='nw', window=startButton)
+about_button_window = menu.create_window(300, 630, anchor='nw', window=aboutButton)
+quit_button_window = menu.create_window(500, 630, anchor='nw', window=exitButton)
 
     
 
